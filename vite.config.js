e07dc689c692f350f582054bc4a5b87f47774078
Moch. Gustav Ali Rafsandjani <mgustav.penting@gmail.com>
2025-03-23 23:19:38 +0700

    fix : custom domain don't need project name anymore

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/digits/",
+  base: "/",
   plugins: [
     vue(),
     vueDevTools(),
